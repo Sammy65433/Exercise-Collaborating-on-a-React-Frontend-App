28b440cf929f05982e896028fd78348f3a02debd
Sammy65433 <samiamlistening@gmail.com>
2026-07-28 13:29:22 -0400

    Exercise: Collaborating on a React Frontend App 502

--- api.js
+++ api.js
@@ -1,23 +1,41 @@
-
-
 const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_URL = "http://www.omdbapi.com"
+const BASE_URL = "https://www.omdbapi.com/";
 
 export async function getItems(query = "batman") {
     try {
-
         const resp = await fetch(`${BASE_URL}?apikey=${API_KEY}&s=${query}`);
         const data = await resp.json();
 
-        console.log(data)
+        console.log("Search results:", data);
+        console.log("Movies array:", data.Search);
 
-        if(data.resp === "False") {
+        if (data.Response === "False") {
             return [];
         }
 
-        return data.Search;
-        
+        return data.Search || [];
     } catch (error) {
-        console.error("Error fetching Data:", error)
+        console.error("Error fetching Movies:", error);
+        return [];
     }
 }
+
+export async function getItemDetails(id) {
+    console.log("ID:", id);
+    try {
+        const resp = await fetch(`${BASE_URL}?apikey=${API_KEY}&i=${id}`);
+        const text = await resp.text();
+        console.log("RAW:", text);
+        const data = JSON.parse(text);
+
+        if (data.Response === "False") { //check if OMDb says the movie was not found 
+            return null;
+        }
+
+        return data;
+    } catch (error) {
+        console.error("Error fetching MovieDetails:", error);
+        return null;
+    }
+}
+
